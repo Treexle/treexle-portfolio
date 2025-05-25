81ccdf1eea98e320562d9e5b641ff26718b4e47f
Treedans <fm418491@gmail.com>
2025-05-25 14:18:44 +0700

    Update Readme & Creating Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div className="flex items-center justify-center mb-4 ">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="font-mono font-bold">
-        <span className="text-yellow-200">Vite</span> +{" "}
-        <span className="text-blue-600">React</span> +{" "}
-        <span className="text-teal-400">TailwindCSS</span>
-      </h1>
-      <div className="card">
-        <button
-          className="bg-slate-900 rounded-lg"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
-      </div>
+    
     </>
   );
 }
